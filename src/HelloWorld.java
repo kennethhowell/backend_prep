9ad2e9253768d37cc4bc249c320ed6fad3c15ca3
kennethhowell <howell.kenneth.a@gmail.com>
2021-04-21 14:39:16 -0500

    feat: created structure to start prepping for backend lectures. worked through Java intro exer

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        System.out.print("Hello ");
+        System.out.print("World!");
+
+
+
+    }
+
+
+}
+
+
