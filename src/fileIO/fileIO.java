0402f94afc9d2f2ba5bbeac2e5f1e06b5d585979
kennethhowell <howell.kenneth.a@gmail.com>
2021-04-30 14:50:23 -0500

    through slide 5 - livecode + comment chain for lecture. broke down concepts as comment string opening PSVM

--- src/fileIO/fileIO.java
+++ src/fileIO/fileIO.java
@@ -8,26 +8,50 @@ import java.util.List;
 
 public class fileIO {
 
-
-
     public static void printFileContents(Path filePath) throws IOException {
         System.out.println();
-        List<String> fileContents = Files.readAllLines(filePath);
+        List<String> fileContents = Files.readAllLines(filePath); //We need List as .readAllLines returns List datatype
         for (int i = 0; i < fileContents.size(); i++) {
             System.out.printf("%d: %s\n", i + 1, fileContents.get(i));
         }
     }
 
     public static void main(String[] args) throws IOException {
-//        Path filename = Paths.get("/Users/kennethhowell/IdeaProjects/backend_prep/src/fileIO/wcw.txt"); //absolute path [used intelliJ 'grab' tool]
-        Path filename = Paths.get("./src/fileIO/wcw.txt"); //relative ./ from current directory
+        //Paths class = static methods to obtain filepaths (Path class)
+        //Path interface = abstracts away differences between Win && MacOS [vs. trying to do everything with strings]
+        //Files class - Files.methods() for manipulating / interacting with files ^ utilizing paths above
+        //File class object{} representation of a file
 
 
-        System.out.println("filename = " + filename); //value of Path object
+//  ** Slide 4
+//  Path filename = Paths.get("/Users/kennethhowell/IdeaProjects/backend_prep/src/fileIO/wcw.txt"); //** absolute path example [used intelliJ 'grab' tool]
+        Path filepath = Paths.get("./src/fileIO/wcw.txt"); //** relative ./ from current directory
 
-        System.out.println("Files.exists(filename) = " + Files.exists(filename)); //bool did you find this filename?
 
-        printFileContents(filename); //method from @ DH lecture, utilizes Files class in above method + for loop to set up a formatted string line by line for output
+        System.out.println("filepath = " + filepath); //value of Path object
+
+        System.out.println("Files.exists(filepath) = " + Files.exists(filepath)); //bool did you find this filename?
+
+//        printFileContents(filepath); //method from @ DH lecture, utilizes Files class in above method + for loop to set up a formatted string line by line for output
+
+// ~~ Slide 5: Creating Dir/File from scratch ~~
+
+        String directory = "./src/fileIO/data"; //~~ relative path to where we want our directory to go
+        String filename = "info.txt"; //~~filename for our desired file
+
+        Path dataDirectory = Paths.get(directory); //Path-ify our string from above
+        Path dataFile = Paths.get(directory, filename); //Path-ify the whole shebang (directory + filename);
+
+        System.out.println("dataFile = " + dataFile); //et voila!
+
+        if(Files.notExists(dataDirectory)) {
+            Files.createDirectories(dataDirectory);
+        }
+
+        if(Files.notExists(dataFile)) {
+            Files.createFile(dataFile);
+        }
+
     }
 
 
