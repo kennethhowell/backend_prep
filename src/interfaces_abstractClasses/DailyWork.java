ffceca6cdfd57c8231e53855a7d44b7846b10a18
kennethhowell <howell.kenneth.a@gmail.com>
2021-04-30 10:58:29 -0500

    basic shell and code created - compiled/ran as expected. future: step back into this package, review structure for delivery, and think of other ways to demonstrate/clarify concepts within the Company example

--- src/interfaces_abstractClasses/DailyWork.java
+++ src/interfaces_abstractClasses/DailyWork.java
@@ -2,6 +2,8 @@ package interfaces_abstractClasses;
 
 public interface DailyWork {
 
+    String businessHours = "9:00 to 5:00, Monday through Friday";
+
     String work();
     String morningMeeting();
     String lunchTime();
